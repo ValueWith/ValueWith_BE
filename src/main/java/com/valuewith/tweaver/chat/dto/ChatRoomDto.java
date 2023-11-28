5fdd836e97b37f36026ef24d39a2157e16deb4c9
eod940 <eod940@gmail.com>
2023-11-29 01:39:56 +0900

    refactor: [036-MESSAGE-REFACTOR] 메시지 생성일자 response에 추가

--- src/main/java/com/valuewith/tweaver/chat/dto/ChatRoomDto.java
+++ src/main/java/com/valuewith/tweaver/chat/dto/ChatRoomDto.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChatRoomDto {
+public class ChatRoomDto{
   private Long chatRoomId;
   private Long tripGroupId;
   private String title;
--- src/main/java/com/valuewith/tweaver/message/dto/MessageDto.java
+++ src/main/java/com/valuewith/tweaver/message/dto/MessageDto.java
@@ -4,6 +4,7 @@ import com.valuewith.tweaver.auditing.BaseEntity;
 import com.valuewith.tweaver.auth.dto.LoginMemberIdDto;
 import com.valuewith.tweaver.member.entity.Member;
 import com.valuewith.tweaver.message.entity.Message;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,12 +17,14 @@ import lombok.NoArgsConstructor;
 public class MessageDto {
   private LoginMemberIdDto memberIdDto;
   private String content;
+  private LocalDateTime created_at;
 
   public static MessageDto from(Message message) {
     Member member = message.getMember();
     return MessageDto.builder()
         .content(message.getContent())
         .memberIdDto(LoginMemberIdDto.from(member))
+        .created_at(message.getCreatedDateTime())
         .build();
   }
 }

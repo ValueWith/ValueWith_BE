ebd6ac4030cadddbd7293f0f06c4db51f0e4a7bf
eod940 <eod940@gmail.com>
2024-01-29 15:31:26 +0900

    test: [TEST-SIGNUP] 회원 가입 테스트 2: 프로필 사진 없을 경우 성공

--- src/test/java/com/valuewith/tweaver/auth/service/AuthServiceUnitTest.java
+++ src/test/java/com/valuewith/tweaver/auth/service/AuthServiceUnitTest.java
@@ -1,15 +1,20 @@
 package com.valuewith.tweaver.auth.service;
 
 import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.valuewith.tweaver.auth.dto.AuthDto;
 import com.valuewith.tweaver.constants.ImageType;
+import com.valuewith.tweaver.defaultImage.entity.DefaultImage;
+import com.valuewith.tweaver.defaultImage.entity.DefaultImage.DefaultImageBuilder;
 import com.valuewith.tweaver.defaultImage.repository.DefaultImageRepository;
 import com.valuewith.tweaver.defaultImage.service.ImageService;
 import com.valuewith.tweaver.member.entity.Member;
 import com.valuewith.tweaver.member.repository.MemberRepository;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -56,6 +61,25 @@ public class AuthServiceUnitTest {
     verify(memberRepository, times(1)).save(any(Member.class));
   }
 
+  @DisplayName("프사 없는 회원가입 성공")
+  @Test
+  void signUpWithoutPicture() {
+    // Given
+    AuthDto.SignUpForm signUpForm = createSignUpForm();
+    DefaultImage defaultImage = createDefaultImage();
+    String password = signUpForm.getPassword();
+
+    // When
+    // DefaultImage로 설정하기로 한 이미지는 DB에 이미 있으므로 이미지를 리턴해준다.
+    when(defaultImageRepository.findByImageName("멤버")).thenReturn(defaultImage);
+    authService.signUp(signUpForm, null);
+
+    // Then
+    verify(passwordEncoder, times(1)).encode(password);
+    verify(defaultImageRepository, times(1)).findByImageName("멤버");
+    verify(memberRepository, times(1)).save(any(Member.class));
+  }
+
   private AuthDto.SignUpForm createSignUpForm() {
     AuthDto.SignUpForm form = new AuthDto.SignUpForm();
     form.setEmail("test@tweaver.com");
@@ -64,5 +88,13 @@ public class AuthServiceUnitTest {
     form.setGender("male");
     form.setAge(20);
     return form;
+  }
+
+  private DefaultImage createDefaultImage() {
+    return DefaultImage.builder()
+        .defaultImageId(1L)
+        .defaultImageUrl("http://test.test")
+        .imageName("test")
+        .createdDateTime(LocalDateTime.now()).build();
   }
 }
